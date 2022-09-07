917b92572ce4bd1b8395b5566ed1b3d2e9246a52
ayuariesta <=>
2022-09-07 19:13:41 +0700

    tambah movie list

---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movie/service/http_service.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  String result = "";
+  late HttpService service;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/service/http_service.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  final String apiKey = 'f0a794bc59c11e75696e0ab4d86f1318';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+  Future<String> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      String response = result.body;
+      return response;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+}
